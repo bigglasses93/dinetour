2774434a8868bfa7b03025ead9f2ef65f21a84bd
秋纫 <qrqiuren@users.noreply.github.com>
2018-12-14 21:31:54 +0900

    Make Sign Up functional

--- src/views/SignUp/index.vue
+++ src/views/SignUp/index.vue
@@ -8,23 +8,27 @@
                 <v-toolbar-title>Sign Up</v-toolbar-title>
               </v-toolbar>
 
+              <!--
               <v-text-field v-model="name" label="name">
               </v-text-field>
+              -->
 
-              <v-text-field v-model="email" label="email">
+              <v-text-field v-model="email" label="Email" :rules="[rules.email]">
               </v-text-field>
 
 
-              <v-text-field type="password" v-model="password" label="password">
+              <v-text-field type="password" v-model="password" label="Password">
               </v-text-field>
 
-              <v-text-field type="confirm password" v-model="confirm_password"
-                label="confirm password">
+              <v-text-field type="password" v-model="confirm_password"
+                label="Confirm Password"
+                :rules="[rules.passwordMatch]">
               </v-text-field>
 
 
               <v-checkbox
-              label="I agree with the terms and conditions."
+                v-model="agree"
+                label="I agree with the terms and conditions."
               ></v-checkbox>               
 
               <v-btn v-on:click="signUp">Sign Up</v-btn> 
@@ -43,21 +47,40 @@ export default {
     return {
       email: null,
       password: null,
-      confirm_password: null
+      confirm_password: null,
+      agree: false,
+      rules: {
+        passwordMatch: () => {
+          return (
+            this.password == this.confirm_password || "Password does not match."
+          );
+        },
+        email: value => {
+          const pattern = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+          return pattern.test(value) || "Invalid e-mail."
+        }
+      }
     };
   },
   methods: {
     signUp: function() {
-      firebase
-        .auth()
-        .createUserWithEmailAndPassword(this.email, this.password)
-        .then(user => {
-          alert("Create account: ", this.email);
-          console.log(user);
-        })
-        .catch(error => {
-          alert(error.message);
-        });
+      if (this.agree && this.password == this.confirm_password) {
+        firebase
+          .auth()
+          .createUserWithEmailAndPassword(this.email, this.password)
+          .then(user => {
+            alert("Create account: ", this.email);
+            console.log(user);
+            this.$router.push("/signin");
+          })
+          .catch(error => {
+            alert(error.message);
+          });
+      } else {
+        alert(
+          "Please check if your password matches and if you agree our terms."
+        );
+      }
     }
   }
 };
