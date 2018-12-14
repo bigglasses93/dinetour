cb09ba268468cf1ab2a3daf5deeacff3a10ccb5e
Anh Nguyen <bigglasses93@gmail.com>
2018-12-14 13:42:30 +0900

    update home

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -3,7 +3,7 @@
     <v-container>
       <v-layout row>
         <v-flex xs12>
-          <h1>PBC #4 TopPage</h1>
+          <h1>Dine Tour</h1>
         </v-flex>
       </v-layout>
     </v-container>
