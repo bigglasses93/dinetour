3f0f95a54d1f975e0306a47a0dacd29fae15c7d0
秋纫 <qrqiuren@users.noreply.github.com>
2018-12-14 16:08:21 +0900

    Update CreateEvent layout

--- src/views/CreateEvent/index.vue
+++ src/views/CreateEvent/index.vue
@@ -1,54 +1,64 @@
 <template>
-  <v-form>
-    <v-container>
-      <v-layout column>
+  <v-content>
+    <v-form>
+      <v-container>
+        <v-layout column justify-center>
 
-        <v-flex xs12>
-          <v-text-field
-            label="Name"
-          ></v-text-field>
-        </v-flex>
+          <v-toolbar dark>
+            <v-toolbar-title>Create Event</v-toolbar-title>
+          </v-toolbar>
 
-        <v-flex xs12>
-          <v-text-field
-            class="mt-0"
-            hide-details
-            single-line
-            type="number"
-            label="Number of People"
-          ></v-text-field>
-        </v-flex>
+          <v-flex xs12 sm8 md4>
+            <v-text-field
+              label="Event Name"
+            ></v-text-field>
+          </v-flex>
 
-        <v-flex xs12 sm6 md3>
-          <v-text-field
-            label="Location"
-            prepend-icon="place"
-          ></v-text-field>
-        </v-flex>
+          <v-flex xs2>
+            <v-text-field
+              class="mt-0"
+              hide-details
+              single-line
+              type="number"
+              label="Number of People"
+            ></v-text-field>
+          </v-flex>
 
-        <v-flex xs12 sm6 md3>
-          <v-date-picker>
-          </v-date-picker>
-        </v-flex>
+          <v-flex xs2>
+            <v-text-field
+              label="Location"
+              prepend-icon="place"
+            ></v-text-field>
+          </v-flex>
 
-        <v-flex xs12 sm6 md3>
-          <v-time-picker></v-time-picker>
-        </v-flex>
+          <v-layout row>
+            <v-flex xs4>
+              <v-date-picker></v-date-picker>
+            </v-flex>
+            <v-flex xs4>
+              <v-time-picker></v-time-picker>
+            </v-flex>
+            <v-spacer></v-spacer>
+          </v-layout>
 
-        <v-flex xs12 sm6 md3>
-          <v-textarea
-            name="description"
-            label="Description"
-          ></v-textarea>
-        </v-flex>
+          <v-flex xs2>
+            <v-textarea
+              name="description"
+              label="Description"
+            ></v-textarea>
+          </v-flex>
 
-        <v-flex xs12 sm6 md3>
-          <v-btn color="primary">Create</v-btn>
-        </v-flex>
-      </v-layout>
-    </v-container>
-  </v-form>
+          <v-flex xs2>
+            <v-btn color="primary">Create</v-btn>
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </v-form>
+  </v-content>
 </template>
 
 <script>
 </script>
+
+<style lang="scss">
+</style>
