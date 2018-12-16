29980e17e1afa442e86657220c0d03ba1289d32c
Anh Nguyen <bigglasses93@gmail.com>
2018-12-16 10:32:14 +0900

    create event push to firebase

--- src/views/CreateEvent/index.vue
+++ src/views/CreateEvent/index.vue
@@ -9,13 +9,14 @@
           </v-toolbar>
 
           <v-flex xs12 sm8 md4>
-            <v-text-field
+            <v-text-field v-model="name"
               label="Event Name"
             ></v-text-field>
           </v-flex>
 
           <v-flex xs2>
             <v-text-field
+              v-model="numberofpeople"
               class="mt-0"
               hide-details
               single-line
@@ -26,6 +27,7 @@
 
           <v-flex xs2>
             <v-text-field
+              v-model="place"
               label="Location"
               prepend-icon="place"
             ></v-text-field>
@@ -33,23 +35,24 @@
 
           <v-layout row>
             <v-flex xs4>
-              <v-date-picker></v-date-picker>
+              <v-date-picker v-model="date"></v-date-picker>
             </v-flex>
             <v-flex xs4>
-              <v-time-picker></v-time-picker>
+              <v-time-picker v-model="time"></v-time-picker>
             </v-flex>
             <v-spacer></v-spacer>
           </v-layout>
 
           <v-flex xs2>
             <v-textarea
+              v-model="description"
               name="description"
               label="Description"
             ></v-textarea>
           </v-flex>
 
           <v-flex xs2>
-            <v-btn color="primary">Create</v-btn>
+            <v-btn color="primary" @click="submit">Create</v-btn>
           </v-flex>
         </v-layout>
       </v-container>
@@ -58,6 +61,42 @@
 </template>
 
 <script>
+import firebase from "firebase";
+
+export default{
+  data: () => ({
+    date: '2019-01-01',
+    time: '00:00',
+    // host: "",
+    // image: "",
+    name: "",
+    numberofpeople: "",
+    place: "",
+    description: ""   
+  }),
+
+  methods: {
+    submit() {
+      var database = firebase.database();
+      console.log("loaded firebase");
+      if (database) {
+        var formData = {
+          date: this.date.toString(),
+          time: this.time,
+          // host: "",
+          // image: this.image,
+          name: this.name,
+          numberofpeople: this.numberofpeople,
+          place: this.place,
+          description: this.description
+        };
+        database.ref("events").push(formData);
+        alert("Created event successfuly.");
+        this.$router.push("/");
+      }
+    }
+  }
+};
 </script>
 
 <style lang="scss">
