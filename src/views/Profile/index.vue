957bc18b12e486cf4f6b4cdba37ab83c9acb599e
冉璐 <nicole@ranludeMacBook.local>
2018-12-16 14:36:06 +0900

    Edit Profile2.0

--- src/views/Profile/index.vue
+++ src/views/Profile/index.vue
@@ -15,7 +15,7 @@
               <v-layout justify-space-between>
                 <!-- <v-btn outline color="black">Information</v-btn> -->
                 <v-dialog v-model="dialog" persistent max-width="600px">
-                <v-btn outline slot="activator" color="black" dark>Information</v-btn>
+                <v-btn outline slot="activator" color="black" dark style="text-decoration:underline">Information</v-btn>
                 <v-card>
                     <v-card-title>
                     <span class="headline" style="font-weight:bold">User Profile</span>
@@ -69,9 +69,9 @@
                     </v-card-actions>
                 </v-card>
                 </v-dialog>
-                <v-btn outline color="black">Events</v-btn>
-                <v-btn outline color="black">Calendar</v-btn>
-                <v-btn outline color="black">Other</v-btn>
+                <v-btn outline color="black"><a href="#_events" style="color:black">Events</a></v-btn>
+                <v-btn outline color="black"><a href="#_calendar" style="color:black">Calendar</a></v-btn>
+                <v-btn outline color="black"><a href="#_other" style="color:black">Other</a></v-btn>
                 <!-- <a href="javascript:;" class="body-2">Information</a>
                 <a href="javascript:;" class="body-2">Events</a>
                 <a href="javascript:;" class="body-2">Calendar</a>
@@ -89,6 +89,7 @@
                   height="170px"
                   :src="post.imgUrl"
                 >
+                <a :id="post.id"></a>
                   <v-container fill-height fluid>
                     <v-layout>
                       <v-flex xs12 align-end d-flex>
@@ -101,22 +102,63 @@
                   {{ post.content }}
                 </v-card-text>
                 <v-card-actions>
-                  <v-btn icon class="red--text">
-                    <v-icon medium>fa-reddit</v-icon>
+                  <v-btn fab dark small color="indigo" flat to="/create-event">
+                    <v-icon dark>add</v-icon>
                   </v-btn>
-                  <v-btn icon class="light-blue--text">
-                    <v-icon medium>fa-twitter</v-icon>
+                  <v-btn flat icon color="cyan">
+                    <v-icon dark>edit</v-icon>
                   </v-btn>
-                  <v-btn icon class="blue--text text--darken-4">
-                    <v-icon medium>fa-facebook</v-icon>
+                  <v-btn flat icon color="primary">
+                    <v-icon dark>remove</v-icon>
                   </v-btn>
                   <v-spacer></v-spacer>
-                  <v-btn flat class="blue--text">Read More</v-btn>
+                  <v-btn flat class="blue--text">Details</v-btn>
                 </v-card-actions>
               </v-card>
             </div>
           </v-flex>
         </v-layout>
+        <v-layout row wrap align-center>
+                <v-flex>
+                  <v-card class="elevation-0 transparent">
+                    <v-card-text class="text-xs-center">
+                      <v-icon x-large class="blue--text text--lighten-2">date_range</v-icon>
+                    </v-card-text>
+                    <v-card-title primary-title class="layout justify-center">
+                      <div class="headline text-xs-center"><a id="_calendar">Calendar</a></div>
+                    </v-card-title>
+                    <v-card-text>
+
+                    </v-card-text>
+                    <v-date-picker
+                    v-model="date"
+                    full-width
+                    landscape
+                    class="mt-3"
+                  ></v-date-picker>
+                  <v-flex class="my-2 px-1">
+                    <div class="title">Month Appointments ({{ pickerDate || 'no appointment' }})</div>
+                    <div class="subheading">Change month to see other appointments</div>
+                    <ul class="ma-3">
+                      <li v-for="note in notes" :key="note">{{ note }}</li>
+                    </ul>
+                  </v-flex>
+                  </v-card>
+                </v-flex>
+                <v-flex>
+                  <v-card class="elevation-0 transparent">
+                    <v-card-text class="text-xs-center">
+                      <v-icon x-large class="blue--text text--lighten-2">flash_on</v-icon>
+                    </v-card-text>
+                    <v-card-title primary-title class="layout justify-center">
+                      <div class="headline"><a id='_other'>Others</a></div>
+                    </v-card-title>
+                    <v-card-text>
+                      Nothing Special here. -3-
+                    </v-card-text>
+                  </v-card>
+                </v-flex> 
+          </v-layout>
       </v-container>
     </v-content>
 </template>
@@ -124,22 +166,25 @@
 <script>
   export default {
     data () {
-      
+      date: new Date().toISOString().substr(0, 10)
       return {
         dialog: false,
         title: 'Your Logo',
         posts: [
           {
+            id:'_events',
             title: 'Fusce ullamcorper tellus',
             content: 'Fusce ullamcorper tellus sed maximus rutrum. Donec imperdiet ultrices maximus. Donec non tellus non neque pellentesque fermentum. Aenean in pellentesque urna.',
             imgUrl: 'https://raw.githubusercontent.com/vuetifyjs/docs/dev/static/doc-images/cards/drop.jpg'
           },
           {
+            // id:'_calendar',
             title: 'Donec vitae suscipit lectus, a luctus diam.',
             content: 'Donec vitae suscipit lectus, a luctus diam. Proin vitae felis gravida, lobortis massa sit amet, efficitur erat. Morbi vel ultrices nisi.',
             imgUrl: 'https://raw.githubusercontent.com/vuetifyjs/docs/dev/static/doc-images/cards/docks.jpg'
           },
           {
+            // id:'_other',
             title: 'Vestibulum condimentum quam',
             content: 'At sagittis sapien vulputate. Vivamus laoreet lacus id magna rutrum dapibus. Donec vel pellentesque arcu. Maecenas mollis odio tempus felis elementum commodo.',
             imgUrl: 'https://raw.githubusercontent.com/vuetifyjs/docs/dev/static/doc-images/cards/plane.jpg'
